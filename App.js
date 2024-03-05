d637dfc767f260b846659f7dbdcbe6fc7c2b5edb
Krzysztof Tys <krzysztof.tys@gmail.com>
2024-03-05 22:34:10 +0100

    add notes react native basics

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Where is my text</Text>
       <StatusBar style="auto" />
     </View>
   );
