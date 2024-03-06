aa76397fd2d165e33facb644f4d81e45b68789c5
Krzysztof Tys <krzysztof.tys@gmail.com>
2024-03-06 18:18:17 +0100

    draft of first view

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
+  const [activities, setActivities] = useState([{ id: 1, name: "aaa" }, { id: 2, name: "bbb" }]);
+
+  const addActivity = () => setActivities(produce(activities, draft => { draft.push({ id: uuidv4() }) }))
+
+  const changeActivity = (activity, value) => {
+    const nextActivities = produce(activities, draftActivities => {
+      const index = draftActivities.indexOf(draftActivity => draftActivity.id == activity.id);
+      draftActivities[index].value = value;
+    });
+
+    setActivities(nextActivities);
+  }
+
+  const renderActivities = () => {
+    return activities.map(activity => (<TextInput key={activity.id} onChange={value => changeActivity(activity, value)} value={activity.value} placeholder="type in activity name" />))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Where is my text</Text>
+      <Text>Hello</Text>
+      {renderActivities}
+      <Button
+        onPress={addActivity}
+        title="+"
+      />
       <StatusBar style="auto" />
     </View>
   );
